4edc4ff080ef8b62826ddfcf725fb862d292fc56
Prabhu <prabhu23cs@gmail.com>
2026-05-28 16:58:03 +0530

    API Changes

---
+++ Employee-Api/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using Employee_Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee_Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private static List<Employee> employees = new()
+        {
+            new Employee { Id = 1, Name = "Prabhu", Role = "Developer" },
+            new Employee { Id = 2, Name = "John", Role = "Tester" }
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Employee employee)
+        {
+            employee.Id = employees.Count + 1;
+
+            employees.Add(employee);
+
+            return Ok(employee);
+        }
+    }
+}
--- Employee-Api/Program.cs
+++ Employee-Api/Program.cs
@@ -7,18 +7,34 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+     {
+         policy.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+     });
+});
+
+
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("AllowAngular");
 
 app.MapControllers();
 
